4061495f532b420800b1a4a797668ee8f17f87cc
Gionandez <gio.nandez23@gmail.com>
2024-10-18 14:32:02 -0500

    Se agrega banner principal y footer con información

--- client/cliente/view/home.js
+++ client/cliente/view/home.js
@@ -1,11 +1,16 @@
 export const home = /*html*/`
+<div class="slider-p">
+    <img class="responsive-img" src="../cliente/assets/Banner-principal.webp">
+</div>
+<div class="container row">
 <div class="card" >
    <div class="card-content">
 <div class="section">
-    <h4>Bienvenidos a RentaloFacil</h4>
-    <h5>Lo tienes tú… Lo quiero yo.</h5>
+    <h4>Bienvenidos a <b>Rentalo Fácil</b></h4>
     <p>Tu sitio donde te facilitamos el proceso de encontrar tu punto ideal.</p>
+    <br>
     <iframe width="853" height="480" src="https://www.youtube.com/embed/dlaScEErrFY" title="¡Encuentra la ubicación perfecta para tu local!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
 </div>
 </div>
 </div>
